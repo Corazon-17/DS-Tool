b943eaa8567519e6d8e3ccb67357483564f7f751
Corazon-17 <65112982+Corazon-17@users.noreply.github.com>
2022-10-02 10:39:23 +0700

    Update build_model.py

--- modules/model/build_model.py
+++ modules/model/build_model.py
@@ -6,7 +6,6 @@ from modules.model import classification
 def build_model(dataset, models):
 	data_opt = dataset.list_name()
 	default_idx = st.session_state["default_dataset_idx"]
-	st.write(len(data_opt))
 
 	col1, col2, col3 = st.columns(3)
 	train_name = col1.selectbox(
